109d4bf13f740e658125c07a9bafb31f1763c6ef
Chhotrakunkhunphovodon <chukizz904@gmail.com>
2024-07-24 09:09:29 +0700

    add eshop

---
+++ e-shop/src/main/java/com/aub/e_shop/EShopApplication.java
@@ -0,0 +1,12 @@
+package com.aub.e_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EShopApplication.class, args);
+	}
+}
