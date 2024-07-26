8649953f4e73551fb308eab9a69baac148e0b599
torngzz <chukizz904@gmail.com>
2024-07-26 14:47:25 +0700

    SQL Insert

--- backend-aub-shop/src/main/resources/data.sql
+++ backend-aub-shop/src/main/resources/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO login_model (username, password) VALUES ('admin', '123');
+INSERT INTO TBL_USER (username, password, email, phone, role, created_by, created_date, updated_by, updated_date) 
+VALUES ('admin', '123', 'phovodonn@gmail.com', '0974059394', 'Admin', 'Programmer', '2024-07-24 00:00:00', 'None', '2024-07-24 00:00:00');
+
+INSERT INTO product_list_model (name, category, price) 
+VALUES ('MSI Katarina', 'Laptop', '25.50');
