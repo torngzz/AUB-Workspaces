843dd6dbe58e73509426bee3e0375fb91a231fe0
torngzz <chukizz904@gmail.com>
2024-07-31 14:30:19 +0700

    jjjjj

--- backend-aub-shop/src/main/java/com/aub/backend_aub_shop/config/SecurityConfig.java
+++ backend-aub-shop/src/main/java/com/aub/backend_aub_shop/config/SecurityConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+
+
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
